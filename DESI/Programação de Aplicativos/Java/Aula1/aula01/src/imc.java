0d9099524322523380c1eee7585ed5cee59436d8
Júlio Henrique Busarello <165197218+JulioBusarello@users.noreply.github.com>
2024-08-19 21:14:39 -0300

    Comentários

--- DESI/Programação de Aplicativos/Java/Aula1/aula01/src/imc.java
+++ DESI/Programação de Aplicativos/Java/Aula1/aula01/src/imc.java
@@ -3,20 +3,28 @@ import java.util.Scanner;
 
 public class imc {
     public static void main(String[] args) throws Exception {
+        // Definindo a língua do código
         Locale.setDefault(Locale.US);
+        // Criando um novo Scanner
         Scanner ler = new Scanner(System.in);
 
+        // Definição de Variável
         double peso, altura, imc;
 
+        // Solicita o peso do usuário
         System.out.print("\nInforme o seu peso (Ex: 100): ");
         peso = ler.nextDouble();
 
+        // Solicita a altura do usuário
         System.out.print("\nInforme sua altura (Ex: 1.90): ");
         altura = ler.nextDouble();
 
+        // Cálcula o IMC
         imc = peso / (altura * altura);
 
+        // Informa o resultado do cálculo
         System.out.printf("Seu imc é: %.2f%n", imc);
-        ler.close();
+
+        ler.close();  // Fecha o scanner
     }
 }
--- DESI/Programação de Aplicativos/Java/Aula1/aula01/src/raiz.java
+++ DESI/Programação de Aplicativos/Java/Aula1/aula01/src/raiz.java
@@ -3,15 +3,20 @@ import java.util.Scanner;
 
 public class raiz {
     public static void main(String[] args) throws Exception {
+        // Definindo a língua do código
         Locale.setDefault(Locale.US);
+        // Criando um novo Scanner
         Scanner ler = new Scanner(System.in);
-        
+        // Definição de Variável
         double valor;
+        
+        // Pede o valor
+        System.out.print("\nInsira o valor: "); 
+        valor = ler.nextDouble();  // Armazena o valor informado
 
-        System.out.print("\nInsira o valor: ");
-        valor = ler.nextDouble();
-
+        // Escreve o resultado
         System.out.printf("O valor da raiz quadrada é: %.2f%n", Math.sqrt(valor));
-        ler.close();
+
+        ler.close(); // Fecha o scanner
     }
 }
