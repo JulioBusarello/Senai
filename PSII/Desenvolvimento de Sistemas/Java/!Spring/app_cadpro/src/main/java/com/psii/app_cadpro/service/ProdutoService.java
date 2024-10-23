4573a66c7f8212a8a762344a2538ffb8eb3760c6
Júlio Henrique Busarello <165197218+JulioBusarello@users.noreply.github.com>
2024-10-23 17:01:37 -0300

    CadPro

---
+++ PSII/Desenvolvimento de Sistemas/Java/!Spring/app_cadpro/src/main/java/com/psii/app_cadpro/service/ProdutoService.java
@@ -0,0 +1,35 @@
+package com.psii.app_cadpro.service;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.psii.app_cadpro.model.Produto;
+import com.psii.app_cadpro.repository.ProdutoRepository;
+
+@Service
+public class ProdutoService {
+    
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public Produto salvarProduto(Produto produto){
+        return produtoRepository.save(produto);
+    }
+
+    public List<Produto> listarProdutos(){
+        return produtoRepository.findAll();
+    }
+
+    public Produto buscarPorId(Long id){
+        Optional<Produto> produto = produtoRepository.findById(id);
+        return produto.orElseThrow(() -> new RuntimeException("Produto não encontrado"));
+    }
+
+    public void deletarPorId(Long id){
+        produtoRepository.deleteById(id);
+    }
+}
