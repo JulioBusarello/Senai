5f04dd9fdb5a28b1afd61df8a073dc006920dd0c
Júlio Henrique Busarello <165197218+JulioBusarello@users.noreply.github.com>
2024-10-28 15:55:12 -0300

    db

--- PSII/Desenvolvimento de Sistemas/Java/!Spring/app_cadpro/productdb.sql
+++ PSII/Desenvolvimento de Sistemas/Java/!Spring/app_cadpro/productdb.sql
@@ -3,20 +3,20 @@ USE productdb;
 
 CREATE TABLE `produto` (
   id bigint NOT NULL AUTO_INCREMENT,
-  categoria varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  descricao varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  fornecedor varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  categoria varchar(255) NOT NULL,
+  descricao varchar(255) NOT NULL,
+  fornecedor varchar(255) NOT NULL,
   imagem LONGBLOB,
-  nome varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  preco double DEFAULT NULL,
-  quantidade int DEFAULT NULL,
+  nome varchar(255) NOT NULL,
+  preco double NOT NULL,
+  quantidade int NOT NULL,
   PRIMARY KEY (`id`)
 );
 
 CREATE TABLE pedido (
 	id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    data_pedido DATE,
-    id_produto BIGINT,
+    data_pedido DATE NOT NULL,
+    id_produto BIGINT NOT NULL,
     CONSTRAINT fk_produto_pedido FOREIGN KEY (id_produto) REFERENCES produto(id)
 );
 
