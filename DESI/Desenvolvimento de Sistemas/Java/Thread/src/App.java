cbae7964bdc3853be3449df1f76060309cf69199
Júlio Henrique Busarello <165197218+JulioBusarello@users.noreply.github.com>
2025-02-05 18:38:42 -0300

    Thread parâmetro nome

--- DESI/Desenvolvimento de Sistemas/Java/Thread/src/App.java
+++ DESI/Desenvolvimento de Sistemas/Java/Thread/src/App.java
@@ -1,15 +1,17 @@
 class MinhaThread extends Thread {
 
     private int tempo;
+    private String nome;
 
-    public MinhaThread(int tempo) {
+    public MinhaThread(String nome, int tempo) {
+        this.nome = nome;
         this.tempo = tempo;
     }
 
     public void run() {
         // Codigo a ser executado pela thread
         for (int i = 0; i < 5; i++) {
-            System.out.println("Executando " + Thread.currentThread().getName() + " Valor do Contador: " + i);
+            System.out.println("Executando " + nome + " Valor do Contador: " + i);
             try {
                 Thread.sleep(tempo);
             } catch (InterruptedException e) {
@@ -21,10 +23,10 @@ class MinhaThread extends Thread {
 
 public class App {
     public static void main(String[] args) throws Exception {
-        MinhaThread thread_1 = new MinhaThread(500);
+        MinhaThread thread_1 = new MinhaThread("Thread #1", 500);
         thread_1.start();
 
-        MinhaThread thread_2 = new MinhaThread(3000);
+        MinhaThread thread_2 = new MinhaThread("Thread #2", 3000);
         thread_2.start();
 
         System.out.println("Programa finalizado!");
