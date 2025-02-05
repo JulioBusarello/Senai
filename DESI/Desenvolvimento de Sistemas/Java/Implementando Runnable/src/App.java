a2f82de1bff83c9fea7e8b6ad5f4f208dbae7843
Júlio Henrique Busarello <165197218+JulioBusarello@users.noreply.github.com>
2025-02-05 19:17:37 -0300

    Runnable com parâmetros

--- DESI/Desenvolvimento de Sistemas/Java/Implementando Runnable/src/App.java
+++ DESI/Desenvolvimento de Sistemas/Java/Implementando Runnable/src/App.java
@@ -1,9 +1,18 @@
 class MinhaRunnable implements Runnable {
+
+    private int tempo;
+    private String nome;
+
+    public MinhaRunnable(String nome, int tempo) {
+        this.tempo = tempo;
+        this.nome = nome;
+    }
+
     public void run() {
         for (int i = 0; i < 5; i++) {
-            System.out.println("Executando " + Thread.currentThread().getName() + " Valor do contador: " + i);
+            System.out.println("Executando " + nome + " Valor do contador: " + i);
             try {
-                Thread.sleep(1000);
+                Thread.sleep(tempo);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -13,11 +22,15 @@ class MinhaRunnable implements Runnable {
 
 public class App {
     public static void main(String[] args) throws Exception {
-        MinhaRunnable minhaRunnable = new MinhaRunnable();
-        Thread thread_1 = new Thread(minhaRunnable);
+        MinhaRunnable runnable1 = new MinhaRunnable("Thread 1", 1500);
+        MinhaRunnable runnable2 = new MinhaRunnable("Thread 2", 4500);
+
+        Thread thread_1 = new Thread(runnable1);
         thread_1.start();
 
-        Thread thread_2 = new Thread(minhaRunnable);
+        Thread thread_2 = new Thread(runnable2);
         thread_2.start();
+
+        System.out.println("Programa finalizado!!!");
     }
 }
