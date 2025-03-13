4e6b487abf9995a2b050ab3f543eb919d3681e86
Júlio Henrique Busarello <165197218+JulioBusarello@users.noreply.github.com>
2025-03-12 21:46:35 -0300

    update: Spring
    fornecedor, lote, data de Fabricação

--- DESI/Desenvolvimento de Sistemas/Spring/AppSmart/src/main/java/com/julio/AppSmart/controller/ProdutoController.java
+++ DESI/Desenvolvimento de Sistemas/Spring/AppSmart/src/main/java/com/julio/AppSmart/controller/ProdutoController.java
@@ -13,10 +13,17 @@ public class ProdutoController {
         double preco = 149.99;
         int quantidade = 15;
 
+        String fornecedor = "JS Malharia";
+        String lote = "CX21-2025";
+        String dataFabricacao = "25-01-2024";
+
         model.addAttribute("id", id);
         model.addAttribute("produto", produto);
         model.addAttribute("preco", preco);
         model.addAttribute("quantidade", quantidade);
+        model.addAttribute("fornecedor", fornecedor);
+        model.addAttribute("lote", lote);
+        model.addAttribute("dataFabricacao", dataFabricacao);
 
         return "produto";
     }
